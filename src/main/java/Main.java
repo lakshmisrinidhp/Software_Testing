522b897efc8dafbed54193488b62a063ab083b9d
PACHABOTLALAKSHMISRINIDH <plakshmisrinidh99@gmail.com>
2024-12-19 19:11:02 +0100

    Initial commit for Software Testing project

---
+++ src/main/java/Main.java
@@ -0,0 +1,300 @@
+///
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    //Author: Daniel Pliego Gonzalez
+    //Description: This function count the length of the string that the user give as an input. Test: testLengthWord()
+    public int lengthWord(String word) {
+        return word.length();
+    }
+
+    //Author: Daniel Pliego Gonzalez
+    //Description: This function count the vowels of the string that the user give as an input. Test: testCountVowels()
+    public int countVowels(String word) {
+        int count=0;
+        char[] wordChars = word.toCharArray();
+
+        for (char chr: wordChars){
+            if(chr=='a'||chr=='A' || chr=='e'||chr=='E' || chr=='i'||chr=='I' || chr=='o'||chr=='O' || chr=='u'||chr=='U'){
+                count=count+1;
+            }
+        }
+        return  count;
+    }
+
+    //Author: Daniel Pliego Gonzalez
+    //Description: This Function count the consonants of the string that the user give as an input. Test: testCountConsonants()
+    public int countConsonants(String word) {
+        int count = 0;
+
+        char[] wordChars = word.toCharArray();
+
+        for (char chr : wordChars) {
+            if (chr != 'a' && chr != 'A' && chr != 'e' && chr != 'E' && chr != 'i' && chr != 'I' && chr != 'o' && chr != 'O' && chr != 'u' && chr != 'U') {
+                count = count + 1;
+            }
+        }
+
+        return count;
+    }
+    //Author:Daniel Pliego Gonzalez
+    //same method of count Consonants with return type as Integer test :countConsonantsINT()
+    public String countConsonantsINT(String word) {
+        int count = 0;
+
+        char[] wordChars = word.toCharArray();
+
+        for (char chr : wordChars) {
+            if (chr != 'a' && chr != 'A' && chr != 'e' && chr != 'E' && chr != 'i' && chr != 'I' && chr != 'o' && chr != 'O' && chr != 'u' && chr != 'U') {
+                count = count + 1;
+            }
+        }
+
+        return String.valueOf(count);
+    }
+    //Author:Naga veera sai ram chikkala
+    //method 1
+    // description: count the uppercases here test: testCountUpperCase()
+    public int countUpperCase(String word){
+        int count=0;
+        char[] wordChars=word.toCharArray();
+        for(char chr: wordChars){
+            if(Character.isUpperCase(chr)){
+                count=count+1;
+            }
+        }
+        return count;
+    }
+    //Author:Naga veera sai ram chikkala
+    //method 2
+    // description: count the uppercases integer with return value StringValue(int)
+    public String countUpperCaseINT(String word){
+        int count=0;
+        char[] wordChars=word.toCharArray();
+        for(char chr: wordChars){
+            if(Character.isUpperCase(chr)){
+                count=count+1;
+            }
+        }
+        return String.valueOf(count);
+    }
+    //Author:Naga veera sai ram chikkala
+    //method 3
+    //description : this method  counts the number of lowercases in a string test: testCountLowerCase()
+    public int countLowerCase(String word){
+        int count=0;
+        char[] wordChars=word.toCharArray();
+        for(char chr: wordChars){
+            if(Character.isLowerCase(chr)){
+                count=count+1;
+            }
+        }
+        return count;
+    }
+    //Author:Naga veera sai ram chikkala
+    //method 4
+    //description: this method counts the number of alphabet A and a test:testCountOnlyA()
+    public int countOnlyA(String word){
+        //temp storing string
+        String _word =word;
+        //intial count
+        int count=0;
+        //temp char type to hold value
+        char tempChar;
+        for (int i=0;i< _word.length();i++){
+            //getting value @ index of i and storing it
+            tempChar = _word.charAt(i);
+            //comparing and counting
+            if (tempChar=='A'||tempChar=='a')
+                count++;
+        }
+        return count;
+    }
+    //Author: lakshmi srinidh
+    //method 1
+    //description: count the number in the given string
+    public int countNumbers(String word){
+        int count =0;
+        char[] wordChars=word.toCharArray();
+        for(char chr: wordChars){
+            if(chr=='1'||chr=='2'||chr=='3'||chr=='4'||chr=='5'||chr=='6'||chr=='7'||chr=='8'||chr=='9'||chr=='0') {
+                count = count + 1;
+
+            }
+        }
+        return count;
+    }
+    //Author: lakshmi srinidh
+    //method 2
+    //description: count the Spaces in the given string
+    public int countSpaces(String word){
+        String _word=word;
+        int count=0;
+        char tempChar;
+        for(int i=0;i< _word.length();i++){
+            tempChar= _word.charAt(i);
+            if(tempChar==' ')
+                count++;
+        }
+        return count;
+    }
+    //Author: lakshmi srinidh
+    //method 3
+    //description: count the special character in the given string
+    public int countSpecialSymbols(String word){
+        int count=0;
+        char[] wordChars = word.toCharArray();
+        for (char chr: wordChars){
+                if(chr =='@'||chr=='!'||chr=='&'||chr==','||chr==':'||chr=='"'||chr==';'||chr=='?'||chr=='/'){
+                    count=count+1;
+                }
+            }
+            return count;
+        }
+        //Author: lakshmi srinidh
+        //method 4
+    // count number for the integration test gives the return value string value (int)
+    public String countNumbersINT(String word){
+        int count =0;
+        char[] wordChars=word.toCharArray();
+        for(char chr: wordChars){
+            if(chr=='1'||chr=='2'||chr=='3'||chr=='4'||chr=='5'||chr=='6'||chr=='7'||chr=='8'||chr=='9'||chr=='0') {
+                count = count + 1;
+
+            }
+        }
+        return String.valueOf(count);
+    }
+    //Author: Sai Nikhil
+    //Description: count words starting with vowels. Test: countWordsStartingWithVowels()
+    //Method 1
+    public int countWordsStartingWithVowels(String sentence){
+
+        //initial count
+        int count = 0;
+
+        //storing value passed from testCase
+        String str = sentence;
+
+        //splitting the word after ever <space> and storing it in a string array
+        String[] splitWord = str.split(" ");
+
+        for (int i = 0; i < splitWord.length; i++) {
+
+            //picking up word @ index i of array and storing first character of it to variable
+            char firstCharactor = splitWord[i].charAt(0);
+
+            switch(firstCharactor) {
+                case 'A':
+                    count++;
+                    break;
+                case 'a':
+                    count++;
+                    break;
+                case 'E':
+                    count++;
+                    break;
+                case 'e':
+                    count++;
+                    break;
+                case 'I':
+                    count++;
+                    break;
+                case 'i':
+                    count++;
+                    break;
+                case 'O':
+                    count++;
+                    break;
+                case 'o':
+                    count++;
+                    break;
+                case 'U':
+                    count++;
+                    break;
+                case 'u':
+                    count++;
+                    break;
+                default:
+                    count = count;
+
+            }
+        }
+
+        return count;
+    }
+    //Author: Sai Nikhil
+    // Method 2
+    //Description: counting number of reverse vowels. Test: reverseVowelCount()
+    public int reverseVowelCount(String vowels){
+
+        String _vowels = vowels;
+
+        //array of char to store every alphabet separately
+        char ch[]=_vowels.toCharArray();
+
+        //to store value in reverse
+        String reverseVowel="";
+
+        //initial value for i should be equal to length of char array length
+        //checking if value of i >= 0 for (for loop) to finish looping
+        //decrement value for i
+
+        for(int i=ch.length-1;i>=0;i--){
+            reverseVowel+=ch[i];
+        }
+
+        //System.out.print(reverseVowel.length());
+
+        //returning the count for vowels string passed and reversed
+        return reverseVowel.length();
+
+    }
+    //Author: Sai Nikhil
+    //Method 3
+    //Description: Returning the count of 'b' Test:countOnlyB()
+    public int countOnlyB(String word){
+       //temp storing string
+       String _word = word;
+       //initial count
+        int count = 0;
+        //temp char type to hold value
+        char tempChar;
+
+        for (int i = 0; i< _word.length(); i++){
+            //getting value @ index of i and storing it
+            tempChar = _word.charAt(i);
+
+            //comparing and counting
+            if(tempChar == 'B' || tempChar == 'b'){
+                count++;
+            }
+        }
+        return count;
+    }
+    //Author: Sai Nikhil
+    //Description: Returning the count of 'b' Test:countOnlyB()
+    public String countOnlyBINT(String word){
+        //temp storing string
+        String _word = word;
+        //initial count
+        int count = 0;
+        //temp char type to hold value
+        char tempChar;
+
+        for (int i = 0; i< _word.length(); i++){
+            //getting value @ index of i and storing it
+            tempChar = _word.charAt(i);
+
+            //comparing and counting
+            if(tempChar == 'B' || tempChar == 'b'){
+                count++;
+            }
+        }
+        return String.valueOf(count);
+    }
+}
+
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,153 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class TestMain {
+
+    public static Main main;
+
+    @BeforeClass
+    public static void setUp() {
+        main = new Main();
+        assertNotEquals(main, null);
+    }
+
+    // UNIT TESTS
+    //Author: Daniel Pliego Gonzalez
+    //Description: This test count the letters of the string that the user give as an input. Function: lengthWord(String word)
+    @Test
+    public void testLengthWord() {
+        main = new Main();
+        assertEquals(5, main.lengthWord("music"));
+    }
+    //Author: Daniel Pliego Gonzalez
+    //Description: This test count the Vowels of the string that the user give as an input. Function: countVowels(String word)
+    @Test
+    public void testCountVowels() {
+       main = new Main();
+       assertEquals(3, main.countVowels("magenta"));
+    }
+    //Author: Daniel Pliego Gonzalez
+    //Description: This test count the consonants of the string that the user give as an input. Function: countConsonants(String word)
+    @Test
+    public void testCountConsonants() {
+        main = new Main();
+        assertEquals(4, main.countConsonants("magenta"));
+    }
+    //INTEGRATION TEST
+    //Author: Daniel Pliego Gonzalez
+    //Description: This Test returns if the lenght of the word is the same os the consonant+vowels
+    @Test
+    public void testVowelsAndConsonants() {
+        main = new Main();
+        assertEquals(main.lengthWord("great"), main.countConsonants("great") + main.countVowels("great"));
+    }
+    //Integration test 2
+    //description : lengthWord() and the countConsonantsINT() integration test
+    @Test
+    public void testlengthandcountConsonants() {
+        main = new Main();
+        assertEquals(1,main.lengthWord( main.countConsonantsINT("great")));
+    }
+    //Author:Naga veera sai ram chikkala
+    //test  case 1
+    // test case on the count uppercase test: testCountUpperCase()
+    @Test
+    public void testCountUpperCase(){
+        main =new Main();
+        assertEquals(2,main.countUpperCase("Hello Man"));
+    }
+    //Author:Naga veera sai ram chikkala
+    //test case 2
+    //this test case consits of the count of lower cases test: testCountLowerCase()
+    @Test
+    public void testCountLowerCase(){
+        main =new Main();
+        assertEquals(4,main.countLowerCase("SaiRam"));
+    }
+    //Author:Naga veera sai ram chikkala
+   //test case 3
+    //this test case consits of the count of the alphabet only A and a   test:testCountOnlyA()
+    @Test
+    public void testCountOnlyA(){
+        main= new Main();
+        assertEquals(2,main.countOnlyA("Hello man where are you?"));
+    }
+    //Author:Naga veera sai ram chikkala
+    //integration testcases
+    //test case 1
+    //Description :test on both lengthword() and uppercaseINT()  Test: testLengthAndUpperCase
+    @Test
+    public void testLengthAndUpperCase(){
+        main =new Main();
+        assertEquals(1,main.lengthWord(main.countUpperCaseINT("Sai")));
+    }
+    //lakshmi srinidh
+    //test case 1
+    //count the numbers in the string test: testcountNumber()
+    @Test
+    public void testcountNumber(){
+        main= new Main();
+        assertEquals(5,main.countNumbers("alpha12345"));
+    }
+    //Author: lakshmi srinidh
+    //test case 2
+    // count the spaces in the given string test : testcountSpaces()
+    @Test
+    public void testcountSpaces(){
+        main=new Main();
+        assertEquals(3,main.countSpaces("i am lakshmi srinidh"));
+    }
+    //Author: lakshmi srinidh
+    //test case 3
+    //count the special symbols in the given string test : testSpecialSymbols()
+    @Test
+    public void testSpecialSymbols(){
+        main=new Main();
+        assertEquals(4,main.countSpecialSymbols("alpha,sre,@/"));
+    }
+    //Author: lakshmi srinidh
+    //integration test case 1
+    // test the integration between countNumbersINT() and countNumbers() test: testintNumbers()
+    @Test
+    public void testintNumbers(){
+        main=new Main();
+        assertEquals(2,main.countNumbers(main.countNumbersINT("123455678912233456")));
+    }
+    //nikhil test case 1
+    //count words starting with vowel
+    @Test
+    public void testCountWordsStartingWithVowels(){
+        main = new Main();
+        assertEquals(1, main.countWordsStartingWithVowels("The quick brown fox jumps over the lazy dog"));
+    }
+    // nikhil test case 2
+    //reverse vowel count test
+    @Test
+    public void testReverseVowelCount(){
+        main = new Main();
+        assertEquals(5,main.reverseVowelCount("aeiou"));
+    }
+    //nikhil test case 3
+    //count only 'b' test
+    @Test
+    public void testCountOnlyB(){
+        main = new Main();
+        assertEquals(1,main.countOnlyB("bat"));
+    }
+    //Integration test 1
+    @Test
+    public void testNumbersAndSpecialSymbols(){
+        main = new Main();
+        assertEquals(main.lengthWord("hewill"), main.countNumbers("1234")+main.countSpecialSymbols("@#,"));
+    }
+    //integration test 2
+    //integration between functions CountonlyBINT() and lenghtWord()
+    @Test
+    public void testLengthandcountonlyB(){
+        main = new Main();
+        assertEquals(1,main.lengthWord(main.countOnlyBINT("Bat and Ball")));
+    }
+}
